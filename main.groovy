98f30f734a19a3ec5f95421ef261e1ee775dd9d1
Pallav Jha <47200652+pallavjha25@users.noreply.github.com>
2023-03-22 12:42:54 -0700

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -3,8 +3,8 @@ def defineParameters() {
     
     //Suppose you have two dropdown fields as Jenkins Parameters, and there is certain one to many relations between value of those fields. e.g. Country & states.
     // In that case you can use below method to create two Active Choice parameters that will dynamically change dropdown options of second field based on the first one.
-    (countryNameParameter, stateNameParameter) = addActiveChoicesDeployableReposParameters('world')
-    //Now define another groovy file addActiveChoicesDeployableReposParameters.groovy, where you have specify different list of Countries.
+    (countryNameParameter, stateNameParameter) = addActiveChoicesDeployableStateParameters('world') // here 'world' is just a paremter that will be used to name the paramter in Jenkins.
+    //Now define another groovy file addActiveChoicesDeployableStateParameters.groovy, where you have specify different list of Countries.
     
     properties([
         parameters([
